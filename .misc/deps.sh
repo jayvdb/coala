570f07020125b7091856e87692391742ce338d6b
John Vandenberg <jayvdb@gmail.com>
2016-12-30 15:35:26 +0700

    CI: Remove Julia
    Continues 4091f262

--- .misc/deps.sh
+++ .misc/deps.sh
@@ -14,20 +14,10 @@ case $CIRCLE_NODE_INDEX in
 esac
 
 # apt-get commands
-if [ "$CIRCLE_BUILD_IMAGE" = "ubuntu-12.04" ]; then
-  sudo add-apt-repository -y ppa:staticfloat/juliareleases
-  sudo add-apt-repository -y ppa:staticfloat/julia-deps
-fi
 sudo apt-get update
 deps="indent libclang1-3.4 r-base"
 deps_ruby_npm="gem nodejs"
-deps_julia="julia"
-deps_julia_packages="Lint"
-sudo apt-get install $deps $deps_ruby_npm $deps_julia
-
-for julia_package in $deps_julia_packages ; do
-  julia -e 'Pkg.add("'$julia_package'")'
-done
+sudo apt-get install $deps $deps_ruby_npm
 
 for dep_version in "${dep_versions[@]}" ; do
   pyenv install -ks $dep_version
