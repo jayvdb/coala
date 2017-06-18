700d9dfb5227bf85aa6caf3b64be7b4fde31373b
John Vandenberg <jayvdb@gmail.com>
2017-06-18 17:35:32 +0700

    Fix script

--- .misc/unsupported_script.sh
+++ .misc/unsupported_script.sh
@@ -5,9 +5,10 @@ set +e
 coverage run setup.py install | tee setup.log
 SETUP_EXIT_CODE=$?
 
-grep 'coala supports only python 3.4 or later' setup.log
+grep -q 'coala supports only python 3.4 or later' setup.log
 GREP_EXIT_CODE=$?
 
-if [[ $SETUP_EXIT_CODE == 0 || $GREP_EXIT_CODE == 0 ]]; then
+# Error if setup.py does not fail, or no lines selected by grep
+if [[ $SETUP_EXIT_CODE == 0 || $GREP_EXIT_CODE == 1 ]]; then
   exit 1
 fi
