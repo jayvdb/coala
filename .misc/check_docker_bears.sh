2cb740ee230bc76348a52d07e2f09e9dcc8d86a2
John Vandenberg <jayvdb@gmail.com>
2017-06-26 13:06:28 +0700

    Test bears in docker

---
+++ .misc/check_docker_bears.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+# Use the script defined by the docker to run tests,
+# as it handles bears which are not supported in the image.
+DOCKER_RAW_REPO="https://raw.githubusercontent.com/coala/docker-coala-base"
+BRANCH_GUESSER="$DOCKER_RAW_REPO/master/guess_branch.py"
+
+wget "$BRANCH_GUESSER"
+
+COALA_BRANCH=$(python guess_branch.py "$SOURCE_BRANCH")
+
+DOCKER_TESTS="$DOCKER_RAW_REPO/$COALA_BRANCH/tests/pytest.sh"
+wget "$DOCKER_TESTS"
+chmod 755 pytest.sh
+
+if [[ "$COALA_BRANCH" == "master" ]]; then
+  DOCKER_TAG="pre"
+elif [[ "${COALA_BRANCH:0:8}" != "release/" ]]; then
+  DOCKER_TAG="${COALA_BRANCH:9}"
+else
+  DOCKER_TAG="$COALA_BRANCH"
+fi
+
+IMAGE_NAME="$DOCKER_REPO:$DOCKER_TAG"
+
+# Use the checked out coala, with latest coala-bears for the branch
+docker run -t -i --volume="$(pwd):/coala" "$IMAGE_NAME" /bin/sh -c "
+  set -e -x;
+  pip3 install -U /coala/;
+  cd /coala-bears;
+  git pull;
+  python3 setup.py install;
+  /coala/pytest.sh;
+"
