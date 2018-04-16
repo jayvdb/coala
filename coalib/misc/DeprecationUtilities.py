f0e84c1c74bbb34e661d5f0f40492ddd41f2c29d
John Vandenberg <jayvdb@gmail.com>
2018-04-16 13:19:13 +0700

    DeprecationUtilities: Remove testfixtures
    The docstring uses package testfixtures in a doctest,
    which means the testing package needs to be installed to
    build the docs.

--- coalib/misc/DeprecationUtilities.py
+++ coalib/misc/DeprecationUtilities.py
@@ -9,19 +9,6 @@ def check_deprecation(param_list):
     :param param_list:
         A dictionary of parameters with their names mapped
         to their values being checked for deprecation.
-
-    >>> from testfixtures import LogCapture
-    >>> from collections import OrderedDict
-    >>> param_list = OrderedDict([('foo', None),
-    ...                           ('bar', 'Random'),
-    ...                           ('baz', 1773)])
-    >>> with LogCapture() as capture:
-    ...     check_deprecation(param_list)
-    ...     print(capture)
-    root WARNING
-      bar parameter is deprecated
-    root WARNING
-      baz parameter is deprecated
     """
     for param_name, param_value in param_list.items():
         if param_value is not None:
