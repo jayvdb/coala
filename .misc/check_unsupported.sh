e5e03c4ab4691570c418e4382df5dce1b54e2938
John Vandenberg <jayvdb@gmail.com>
2018-07-17 13:48:11 +0700

    check_unsupported.sh: Fix bugs
    Sync with coala-bears script

--- .misc/check_unsupported.sh
+++ .misc/check_unsupported.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+# Non-zero exit code is what we want to check
 set +e
 
-# Enable capturing the non-zero exit status of coverage instead of tee
+# Enable capturing the non-zero exit status of setup.py instead of tee
 set -o pipefail
 
-coverage run setup.py install | tee setup.log
+set -x
+
+python setup.py install | tee setup.log
 
 retval=$?
 
+set +x
+
 # coalib.__init__.py should exit with 4 on unsupported versions of Python
+# But bears setup.py sees retval 1.
 if [[ $retval != 4 ]]; then
-  echo "Unexpected error code $?"
+  echo "Unexpected error code $retval"
 
   # When the exit code is 0, use a non-zero exit code instead
   if [[ $retval == 0 ]]; then
