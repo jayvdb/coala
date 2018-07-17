21b93e7b1bc9face1ea9bee05b6f15926523d698
John Vandenberg <jayvdb@gmail.com>
2018-07-17 14:33:33 +0700

    One more

--- .misc/check_unsupported.sh
+++ .misc/check_unsupported.sh
@@ -12,7 +12,7 @@ python setup.py install 2>&1 | tee setup.log
 
 retval=$?
 
-set -x
+set +x
 
 # coalib.__init__.py should exit with 4 on unsupported versions of Python
 # But bears setup.py sees retval 1.
@@ -31,7 +31,5 @@ set -e
 
 # The following is emitted on stdout
 grep -q 'coala supports only python 3.4.4 or later' setup.log
-# The following is emitted on stderr
-grep -q 'error: Setup script exited with 4' setup.log
 
 echo "Unsupported check completed successfully"
