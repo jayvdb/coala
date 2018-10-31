f05459eb4893e6b785e405e0154a39f0e934e81d
John Vandenberg <jayvdb@gmail.com>
2018-10-31 20:55:32 +0700

    Linter: Log warning when linter has non-zero exit
    A non-zero exit code from a linter is vital information
    when debugging a bear that isnt working correctly.
    
    Closes https://github.com/coala/coala/issues/27

--- coalib/bearlib/abstractions/Linter.py
+++ coalib/bearlib/abstractions/Linter.py
@@ -683,35 +683,43 @@ def _create_linter(klass, options):
 
                 stdout, stderr = result
 
-                if not options['use_stdout'] and stdout:
+                output = []
+
+                if options['use_stdout']:
+                    output.append(stdout)
+                elif stdout:
                     logging.warning(
                         '{}: Discarded stdout: {}'.format(
                             self.__class__.__name__, stdout))
                     stdout = ''
 
-                if not options['use_stderr'] and stderr:
+                if options['use_stderr']:
+                    output.append(stderr)
+                elif stderr:
                     logging.warning(
                         '{}: Discarded stderr: {}'.format(
                             self.__class__.__name__, stderr))
                     stderr = ''
 
-                output = None
-                if stdout and stderr:
-                    output = stdout, stderr
-                elif stdout:
-                    output = stdout,
-                elif stderr:
-                    output = stderr,
-                else:
-                    logging.info(
-                        '{}: No output; skipping processing'.format(
-                            self.__class__.__name__))
-                    return []
+                if result.code:
+                    logging.warning(
+                        '{}: Exit code {}'.format(
+                            self.__class__.__name__, result.code))
 
                 if options['strip_ansi']:
                     output = tuple(map(strip_ansi, output))
+                else:
+                    output = tuple(output)
+
                 if len(output) == 1:
                     output = output[0]
+
+                if not any([stdout, stderr]):
+                    logging.info(
+                        '{}: No output; skipping processing'.format(
+                            self.__class__.__name__))
+                    return output
+
                 process_output_kwargs = FunctionMetadata.filter_parameters(
                     self._get_process_output_metadata(), kwargs)
                 return self.process_output(output, filename, file,
