aacfb82b83136524679af03e510aa255c3f34b36
John Vandenberg <jayvdb@gmail.com>
2019-02-04 09:46:25 +0700

    Debug

--- .misc/check_moban.sh
+++ .misc/check_moban.sh
@@ -5,7 +5,7 @@ set -ex
 : "${MOBAN_BRANCH:=master}"
 
 git clone https://gitlab.com/coala/mobans \
-          --branch=${MOBAN_BRANCH} ../coala-mobans
+          --branch="${MOBAN_BRANCH}" ../coala-mobans
 
 moban
 git diff --exit-code
