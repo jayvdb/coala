a0279a48c3ff1a96e2c61cb84cc245af5f380874
John Vandenberg <jayvdb@gmail.com>
2019-02-04 10:53:39 +0700

    Sync .misc

--- .misc/check_unsupported.sh
+++ .misc/check_unsupported.sh
@@ -8,6 +8,11 @@ set -o pipefail
 
 set -x
 
+{% if package_module == 'bears' %}
+# Many bear dependencies do not install on unsupported versions
+echo "" > bear-requirements.txt
+
+{% endif %}
 python setup.py install 2>&1 | tee setup.log
 
 retval=$?
@@ -16,7 +21,11 @@ set +x
 
 # coalib.__init__.py should exit with 4 on unsupported versions of Python
 # But bears setup.py sees retval 1.
+{% if package_module == 'coalib' %}
 if [[ $retval != 4 ]]; then
+{% else %}
+if [[ $retval != 1 ]]; then
+{% endif %}
   echo "Unexpected error code $retval"
 
   # When the exit code is 0, use a non-zero exit code instead
@@ -30,6 +39,10 @@ fi
 set -e
 
 # The following is emitted on stdout
-grep -q 'coala supports only Python 3.4.2 or later' setup.log
+grep -q 'coala supports only python 3.4.4 or later' setup.log
+{% if package_module != 'coalib' %}
+# The following is emitted on stderr
+grep -q 'error: Setup script exited with 4' setup.log
+{% endif %}
 
 echo "Unsupported check completed successfully"
